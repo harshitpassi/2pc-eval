34713f3bfd62809f23ba81b87e48e6af229cbee9
Harshit Passi <hrshtpassi@gmail.com>
2020-04-22 15:18:31 -0400

    added documentation to server

--- server.py
+++ server.py
@@ -1,11 +1,12 @@
 from bottle import run, route, get, post, request, template, response
 from pymongo import MongoClient
 
+# Create a connection with local mongo db to serve as key-value store
 client = MongoClient("mongodb+srv://test:test@cluster0-cntdv.mongodb.net/test?retryWrites=true&w=majority&authSource=admin")
 db = client.get_database('test')
 collection = db.get_collection('store-1')
 
-# Handle kv creation
+# Write protocol => if entry exists, update it, otherwise create a new key-value pair
 @post('/kv/write')
 def create_kv():
     data = request.query
@@ -17,7 +18,7 @@ def create_kv():
         collection.find_one_and_update({'key': int(data['key'])}, {'$set': {'value': data['value'], 'ts': int(data['ts'])}})
         return 0
 
-
+# Read protocol => If key exists, send an item, otherwise send a blank object
 @get('/kv/read/<key>')
 def read_kv(key):
     result = collection.find_one({'key': int(key)}, {'_id': 0})
@@ -25,9 +26,10 @@ def read_kv(key):
         return {}
     return result
 
+# Delete protocol => Used to delete existing entries
 @route('/kv/del/<key>')
 def delete_handler(key):
-    collection.delete_one({'key': key})
+    collection.delete_one({'key': int(key)})
     return 0
 
 run(host='localhost', port=60000)
